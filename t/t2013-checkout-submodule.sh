9ec94ab70ca5a6a25304abd51a31f10bc9208655
Jens Lehmann <Jens.Lehmann@web.de>
2010-04-08 22:50:35 +0200

    PRELIMINARY COMMIT

--- t/t2013-checkout-submodule.sh
+++ t/t2013-checkout-submodule.sh
@@ -35,7 +35,7 @@ test_expect_success '"checkout --ignore-submodules <submodule>" updates the inde
 	git diff-index --quiet --cached HEAD &&
 	git checkout --ignore-submodules HEAD^ submodule &&
 	test_must_fail git diff-files --quiet &&
-	git checkout HEAD submodule &&
+	git checkout --ignore-submodules HEAD submodule &&
 	git diff-files --quiet
 '
 
@@ -44,6 +44,52 @@ test_expect_success '"checkout <submodule>" updates recursively' '
 	git diff-files --quiet &&
 	git diff-index --quiet --cached HEAD &&
 	git checkout HEAD^ submodule &&
+	git diff-files --quiet &&
+	git checkout HEAD submodule &&
+	git diff-files --quiet
+'
+
+test_expect_success '"checkout --ignore-submodules" updates the index only' '
+	git checkout master &&
+	git diff-files --quiet &&
+	git diff-index --quiet --cached HEAD &&
+	git checkout --ignore-submodules HEAD^ &&
+	test_must_fail git diff-files --quiet &&
+	git checkout --ignore-submodules master &&
+	git diff-files --quiet
+'
+
+test_expect_success '"checkout" updates recursively' '
+	git checkout master &&
+	git diff-files --quiet &&
+	git diff-index --quiet --cached HEAD &&
+	git checkout HEAD^ &&
+	git diff-files --quiet &&
+	git checkout master &&
+	git diff-files --quiet
+'
+
+test_expect_success '"checkout <submodule>" must use force when submodule contains modified content' '
+	git checkout -f master &&
+	git diff-files --quiet &&
+	git diff-index --quiet --cached HEAD &&
+	echo second >>submodule/first.t
+	git checkout HEAD^ submodule &&
+	test_must_fail git diff-files --quiet &&
+	git checkout -f HEAD^ submodule &&
+	git diff-files --quiet &&
+	git checkout HEAD submodule &&
+	git diff-files --quiet
+'
+
+test_expect_success '"checkout <submodule>" must use force when submodule contains untracked content' '
+	git checkout -f master &&
+	git diff-files --quiet &&
+	git diff-index --quiet --cached HEAD &&
+	echo second >>submodule/second.t
+	git checkout HEAD^ submodule &&
+	test_must_fail git diff-files --quiet &&
+	git checkout -f HEAD^ submodule &&
 	git diff-files --quiet &&
 	git checkout HEAD submodule &&
 	git diff-files --quiet
