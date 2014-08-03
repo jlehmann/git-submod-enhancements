303fe4da89188a8f7036d9fd134541d769cb1a52
Jens Lehmann <Jens.Lehmann@web.de>
2014-08-03 16:55:11 +0200

    fixup! submodule: teach unpack_trees() to repopulate submodules

--- unpack-trees.c
+++ unpack-trees.c
@@ -1448,8 +1448,8 @@ static int check_ok_to_remove(const char *name, int len, int dtype,
 	}
 
 	/*
-	 * If the path lies inside a to be added submodule it
-	 * is ok to remove it.
+	 * If the path lies inside a to be removed submodule it is ok to
+	 * replace it.
 	 */
 	name_copy = xstrdup(name);
 	while ((separator = strrchr(name_copy, '/'))) {
@@ -1457,9 +1457,28 @@ static int check_ok_to_remove(const char *name, int len, int dtype,
 		*separator = '\0';
 		for (i = 0; i < the_index.cache_nr; i++) {
 			struct cache_entry *ce = the_index.cache[i];
-			if (!strcmp(ce->name, name_copy) && S_ISGITLINK(ce->ce_mode)) {
-				free(name_copy);
-				return 0;
+			if (!strcmp(ce->name, name_copy) &&
+			    S_ISGITLINK(ce->ce_mode)) {
+				if (submodule_needs_update(ce->name) &&
+				    submodule_uses_gitfile(ce->name)) {
+					/*
+					 * Submodule will be removed from
+					 * work tree, all files in it will be
+					 * gone.
+					 */
+					free(name_copy);
+					return 0;
+				} else {
+					/*
+					 * Files in a submodule that will not
+					 * be removed must not be presumed
+					 * absent, as everything inside the
+					 * submodule isn't under control of
+					 * this repo.
+					 */
+					name_copy[0] = '\0';
+					break;
+				}
 			}
 		}
 	}
