58e3f8cee0ade40f0a994fc39ce043c867a0de9a
Jens Lehmann <Jens.Lehmann@web.de>
2014-10-15 22:33:55 +0200

    !!! WIP !!! use recursive tests
    Still missing are:
    - t3906-stash-submodule.sh
    - t4255-am-submodule.sh

--- t/t7613-merge-submodule.sh
+++ t/t7613-merge-submodule.sh
@@ -15,5 +15,16 @@ test_submodule_switch "git merge --ff-only"
 KNOWN_FAILURE_NOFF_MERGE_DOESNT_CREATE_EMPTY_SUBMODULE_DIR=1
 KNOWN_FAILURE_NOFF_MERGE_ATTEMPTS_TO_MERGE_REMOVED_SUBMODULE_FILES=1
 test_submodule_switch "git merge --no-ff"
+KNOWN_FAILURE_NOFF_MERGE_DOESNT_CREATE_EMPTY_SUBMODULE_DIR=
+KNOWN_FAILURE_NOFF_MERGE_ATTEMPTS_TO_MERGE_REMOVED_SUBMODULE_FILES=
+
+test_submodule_recursive_switch "git merge --recurse-submodules"
+
+test_submodule_recursive_switch "git merge --recurse-submodules --ff"
+
+test_submodule_recursive_switch "git merge --recurse-submodules --ff-only"
+
+KNOWN_FAILURE_NOFF_MERGE_ATTEMPTS_TO_MERGE_REMOVED_SUBMODULE_FILES=1
+test_submodule_recursive_switch "git merge --recurse-submodules --no-ff"
 
 test_done
